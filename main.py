28e275a8e387b2e1201762fd69e3b1b676ae91a0
Ahmad Alismail <46696930+ahmad-alismail@users.noreply.github.com>
2023-08-04 13:30:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Initialize session state variables
+if 'openai_api_key' not in st.session_state:
+	st.session_state.openai_api_key = ""
+
+if 'serper_api_key' not in st.session_state:
+	st.session_state.serper_api_key = ""
+
+st.set_page_config(page_title="Home", page_icon="🦜️🔗")
+
+st.header("Welcome to LangChain! 👋")
